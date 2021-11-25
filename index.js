8a2e7ef399ecc1cd6fb0239fd68cd556c488b40b
KUNAL  BISWAS <55825231+kunal34@users.noreply.github.com>
2021-11-26 00:58:39 +0530

    Error solved
    Validation added for negative numbers and converted input string to numbers.

--- index.js
+++ index.js
@@ -5,17 +5,24 @@ const checkButton = document.querySelector("#check-button");
 const outputBox = document.querySelector("#message");
 
 checkButton.addEventListener("click", () => {
-    let ip = initialPrice.value;
-    let qty = stockQuantity.value;
-    let curr = currentPrice.value;
+    let ip = Number(initialPrice.value);
+    let qty = Number(stockQuantity.value);
+    let curr = Number(currentPrice.value);
     check(ip, qty, curr);
 });
 
 function check(ip, qty, curr) {
-    console.log(ip.length, qty, curr);
-    if(ip.length === 0 || qty.length === 0 || curr.length === 0 )  {
-        showOutput('Please enter all fields');
-    }else {
+    console.log(ip, qty, curr);
+    if(ip === 0 || qty === 0 || curr === 0 )  {
+        showOutput('Please enter all fields  🥴');
+        outputBox.style.backgroundColor = 'orange';
+    
+    }else if (ip < 0 || qty < 0 || curr < 0) {
+        showOutput('Please enter valid numbers in all fields  🙄');
+        outputBox.style.backgroundColor = 'orange';
+    }
+    else {
+        
         calculateProfitAndLoss(ip, qty, curr);
     }
 }
@@ -24,18 +31,21 @@ function check(ip, qty, curr) {
 
 
 function calculateProfitAndLoss(initial, quantity, current) {
+    
     if (initial > current) {
         let loss = ((initial - current) * quantity).toFixed(2);
         let lossPercentage = ((loss / initial) * 100).toFixed(2);
         outputBox.style.backgroundColor = 'red';
-        showOutput(`The loss is ${loss} and the loss percent is ${lossPercentage}%`);
+        showOutput(`The loss is ${loss} and the loss percent is ${lossPercentage}%  😞`);
     } else if (current > initial) {
+        
         let profit = ((current - initial) * quantity).toFixed(2);
         let profitPercentage = ((profit / initial) * 100).toFixed(2);
         outputBox.style.backgroundColor = 'green';
-        showOutput(`The profit is ${profit} and the profit percent is ${profitPercentage}`);
+        showOutput(`The profit is ${profit} and the profit percent is ${profitPercentage} 😃`);
     } else {
-        showOutput(`No Pain No Gain`);
+        showOutput(`No Profit No Loss  😎`);
+        outputBox.style.backgroundColor = 'Orange';
     }
 }
 
